aa36d61915dd50c5a6181b5557825fac9353283f
J7 <J7DevWeb@gmail.com>
2022-01-29 09:57:03 +0100

    Reactive Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularReactiveForm';
+  ranks =[
+    {key: 'colonel', value: 'Colonel'},
+    {key: 'capitain', value: 'Capitain'}
+  ];
+  
+  characterForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.characterForm = this.formBuilder.group({
+      name: formBuilder.control('', [
+        Validators.required, // ? doc : https://angular.io/guide/form-validation
+        Validators.minLength(3),
+        Validators.maxLength(10),
+        Validators.pattern('^[a-zA-Z]*$')
+      ]),
+      birthday: formBuilder.control(''), // Ajouter des validations
+      rank: formBuilder.control(''),
+      xp: formBuilder.control('0') // 0 sert de valeur pas défaut
+    })
+  }
+
+  public onSubmit(){ 
+    console.log(this.characterForm.value); // Utile pour débugger
+   }
+
+   public control(name:string){
+     return this.characterForm.get(name);
+   }
+
 }
